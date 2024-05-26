3681283226f32a978df3c5a653a5f1d721bd8298
Melisha <melishadjayawardana@gmail.com>
2024-05-27 00:28:46 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["user_id"])) {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = "SELECT * FROM user
+            WHERE id = {$_SESSION["user_id"]}";
+            
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    
+    <header>Home</header>
+    
+    <?php if (isset($user)): ?>
+        
+        <div class="container">
+            <div class="p-style">
+                <p>Hello <?= htmlspecialchars($user["name"]) ?></p>          
+            </div>
+        
+            <div class="button-container">
+                <a href="logout.php">
+                    <input type="submit" class="log-out" value="Log Out">
+                </a>
+            </div>
+        </div>
+
+    <?php else: ?>
+        <div class="button-container">
+            <a href="login.php">
+                <input type="submit" class="log-out" value="Log In">
+            </a>
+            <div class="p-style"><p>-- or --</p></div>
+            <a href="signup.html">
+            <input type="submit" class="log-out" value="Sign Up">
+            </a>
+    <?php endif; ?> 
+    
+    
+</body>
+</html>  
---
+++ login.php
@@ -0,0 +1,84 @@
+<?php
+
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = sprintf("SELECT * FROM user
+                    WHERE email = '%s'",
+                   $mysqli->real_escape_string($_POST["email"]));
+    
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+    
+    if ($user) {
+        
+        if (password_verify($_POST["password"], $user["password_hash"])) {
+            
+            session_start();
+            
+            session_regenerate_id();
+            
+            $_SESSION["user_id"] = $user["id"];
+            
+            header("Location: index.php");
+            exit;
+        }
+    }
+    
+    $is_invalid = true;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+
+<div class="register-container" id="register">
+    
+    <header>Login</header>
+    
+    <div class="form-box">
+    
+        <form method="post">
+
+            <div class="input-box">
+                <input type="email" name="email" id="email" placeholder="Email" class="input-field"
+                    value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+                <i class="bx bx-user"></i>
+            </div>
+        
+            <div class="input-box">
+                <input type="password" name="password" id="password" placeholder="Password" class="input-field">
+                <i class="bx bx-lock-alt"></i>
+            </div>
+        
+            <div class="input-box">
+                <input type="submit" class="submit" value="Log In">
+            </div>
+
+        </form>
+
+    </div>
+</div>
+    
+</body>
+</html>
+
+
+
+
+
+
+
